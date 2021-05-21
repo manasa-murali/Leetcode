08aac5d8cb4a05efaacfb21c8a3e98d451dead44
manasa-5113 <manasa.m@zohocorp.com>
2021-05-21 20:30:00 +0530

    Valid Paranthesis

---
+++ src/ValidParanthesis.kt
@@ -0,0 +1,47 @@
+import java.util.*
+
+object ValidParanthesis {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val strs = ""
+        val isValid = isValid(strs)
+        println(isValid)
+    }
+
+     fun isValid(strs: String): Boolean {
+
+        val stack = Stack<Char>()
+
+        if (strs.isEmpty()) return false
+        for (str in strs) {
+            if (str == '('  ||str == '{'  ||str == '['  ){
+                stack.push(str)
+            }else {
+                if (stack.isNotEmpty()) {
+                    if (str == ')') {
+                        if (stack.peek() == '(') {
+                            stack.pop()
+                        } else {
+                            return false
+                        }
+
+                    } else if (str == '}') {
+                        if (stack.peek() == '{') {
+                            stack.pop()
+                        } else {
+                            return false
+                        }
+
+                    } else if (str == ']') {
+                        if (stack.peek() == '[') {
+                            stack.pop()
+                        } else {
+                            return false
+                        }
+                    }
+                }else return false
+            }
+        }
+        return stack.isEmpty()
+    }
+}
