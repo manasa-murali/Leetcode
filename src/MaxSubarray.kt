f54c21055f992602e89aa07dbb9ba9a1b0792d18
manasa-5113 <manasa.m@zohocorp.com>
2021-05-26 02:03:50 +0530

    Max Subarray

---
+++ src/MaxSubarray.kt
@@ -0,0 +1,34 @@
+object MaxSubarray {
+    @JvmStatic
+    fun main(ars: Array<String>){
+        println(maxSubArray(arrayOf(-2,1,-3,4,-1,2,1,-5,4).toIntArray()))
+    }
+
+    fun maxSubArray(nums: IntArray):Int{
+        var ans=0
+        var sum =0
+        var isNegative= true
+        for (i in nums.indices){
+            if (nums[i]<0 && isNegative){
+                ans = if (i==0) nums[i] else Math.max(ans,nums[i])
+            }else{
+                isNegative = false
+                ans = 0
+                break
+            }
+        }
+        if (isNegative){
+            return ans
+        }
+        for (i in nums.indices){
+            if (sum + nums[i]> 0){
+                sum+=nums[i]
+            }else{
+                sum=0
+            }
+            ans = Math.max(ans,sum)
+        }
+        return ans
+    }
+
+}
