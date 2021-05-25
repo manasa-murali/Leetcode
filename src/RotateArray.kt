2a5fe72acee7a2d70a0c0867836e9f38cf40e6d5
manasa-5113 <manasa.m@zohocorp.com>
2021-05-25 20:45:20 +0530

    Rotate Array Right k times

---
+++ src/RotateArray.kt
@@ -0,0 +1,33 @@
+object RotateArray {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val arrayOf = arrayOf(1, 2)
+        val rotateBy = 3
+        val result = rotate(arrayOf.toIntArray(), rotateBy)
+        println(result.contentToString())
+
+    }
+
+    fun rotate(array: IntArray, rotateBy: Int): IntArray {
+        if (array.isEmpty() || array.size == rotateBy ){
+            return array
+        }
+        val k= Math.floorMod(rotateBy,array.size)
+        reverse(array,array.size-k,array.size-1)
+        reverse(array,0,array.size-k-1)
+        reverse(array,0,array.size-1)
+        return array
+    }
+
+    private fun reverse(array: IntArray, start: Int, end: Int) {
+        var i = start
+        var j= end
+        while (i<j){
+            val temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+            i++
+            j--
+        }
+    }
+}
