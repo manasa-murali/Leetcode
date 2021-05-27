632fef349ea9f44027d728a5970857988c2dbde5
manasa-5113 <manasa.m@zohocorp.com>
2021-05-28 01:43:41 +0530

    Plus One

---
+++ src/PlusOne.kt
@@ -0,0 +1,31 @@
+import com.sun.org.apache.bcel.internal.generic.IFEQ
+
+object PlusOne {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val result = plusOne(arrayOf(1,0,0,0,0).toIntArray())
+        println(result.contentToString())
+    }
+
+    fun plusOne(array: IntArray): IntArray {
+        val arrayList = array.toMutableList()
+        var carry = 0
+        for (i in array.size - 1 downTo 0) {
+            var add = if (i == array.size - 1) 1 else 0
+            if ((arrayList[i] + add + carry)!=0 && (arrayList[i] + add + carry) % 10 == 0) {
+                arrayList[i] = (arrayList[i] + add + carry) % 10
+                if (i == 0) {
+                    carry = 1
+                    arrayList.add(0, carry)
+                }
+                carry = 1
+            } else {
+                arrayList[i] = (arrayList[i] + add + carry) % 10
+                carry = 0
+            }
+        }
+        return arrayList.toIntArray()
+    }
+
+
+}
