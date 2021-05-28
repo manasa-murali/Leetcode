23fc74cf2de1e8d280bf6f4e66a30664ac319852
manasa-5113 <manasa.m@zohocorp.com>
2021-05-28 19:02:02 +0530

    sqrt

---
+++ src/Sqrt.kt
@@ -0,0 +1,38 @@
+object Sqrt {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(mySqrt(5))
+    }
+
+    fun mySqrt(s: Int): Int {
+        return calculate(1L, s.toLong(), s.toLong()).toInt()
+    }
+
+    private fun calculate(min: Long, max: Long, num: Long): Long {
+        if (num==0L){
+            return 0
+        }
+        if (min >= max) {
+            return min
+        } else {
+            val mid = (min + max) / 2
+            if (min * min == num) {
+                return min
+            } else if (max * max == num) {
+                return max
+            } else if (mid * mid == num) {
+                return mid
+            } else if (mid * mid > num) {
+                return calculate(min, mid - 1, num)
+            } else {
+                if (max * max < num) {
+                    return max
+                } else {
+                    return calculate(mid, max-1, num)
+                }
+            }
+        }
+    }
+
+
+}
