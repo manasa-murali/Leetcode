25c3c050845e8c35b83b5c1b9614130b9557f7e2
manasa-5113 <manasa.m@zohocorp.com>
2021-06-06 23:52:08 +0530

    LinkedList loop

--- src/LinkedListCycle.kt
+++ src/LinkedListCycle.kt
@@ -1,19 +1,19 @@
 fun main(){
     val l1_1 = ListNode(3)
-    val l1_2 = ListNode(2)
-    val l1_3 = ListNode(0)
-    val l1_4 = ListNode(-4)
-    l1_1.next = l1_2
-    l1_2.next = l1_3
-    l1_3.next = l1_4
-    l1_4.next = l1_2
+    l1_1.next =null
     println(hasCycle(l1_1))
 }
 fun hasCycle(head: ListNode?): Boolean {
     var walker = head
     var runner = head
+    if(runner!=null && runner.next== null){
+        return false
+    }
     while (walker!=null && runner!!.next!=null){
         walker = walker.next
+        if(runner.next!!.next == null){
+            return false
+        }
         runner = runner.next!!.next
         if (runner == walker) return true
     }
